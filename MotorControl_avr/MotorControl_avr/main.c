11fdf595b6b3b3f965d93cdc53100103b911b485
AS-MOH-NOK <a.mohamma.nok@gmail.com>
2025-07-19 14:43:30 +0330

    enable keypad with inttrupt

--- MotorControl_avr/MotorControl_avr/main.c
+++ MotorControl_avr/MotorControl_avr/main.c
@@ -19,7 +19,12 @@ char key = '0';
 
 void Config();
 
-
+ISR(INT0_vect)
+{
+	key = returnKey();
+//	while(!keyRelease());
+//    PORTA ^= (1 << 3);   // Toggle PA3
+}
 
 int main(void)
 {
@@ -27,24 +32,45 @@ int main(void)
 	keypadConfig();
 	
 	
+
+	
+	/*sbi(DDRA, 3);  //set output PORTC.3 for led
+	cbi(PORTA, 3);  //set led off
+	*/
+	cbi(DDRD, 2);	//set input PORTD.3 for button
+	sbi(PORTD, 2);	//set pull-up
+   
+	
+	GICR = (1<<INT0);	//enable intrppt pin
+	MCUCR = 0;//set intruppt on changes
+	MCUCSR = 0;
+	
+	GIFR = (1<<INTF0);  //be sure puplic interuppt flag is enable
+	sei();
+	
+
+	
+	
     while (1) 
     {
-		//keypadInit();
-		//_delay_ms(20);
-		if(keyPress())
-		{
-			key = returnKey();
+		keypadInit();
+		_delay_ms(20);
+//		if(keyPress())
+//		{
+//			key = returnKey();
 			seg_Port = key;  
 			_delay_ms(20); //stable
-			while(!keyRelease());
-		}
+//			while(!keyRelease());
+//		}
 		//_delay_ms(20); //stable
     }
 }
 
 void Config()
 {
-
+	/*//inttrupt pin config
+	cbi(DDRD, 2);
+	sbi(PORTD, 2);*/
 	//7segment
 	seg_DDR = 0xFF;  //output
 	seg_Port = 0xFF;
